35c3d695ff69c89bb53ebd16830ae27d423e8f87
Jaron <jaronritter01@gmail.com>
2023-04-15 13:56:15 -0500

    add sql files

---
+++ src/main/java/com/finalproject/demeter/sql/clean_reset_db.sql
@@ -0,0 +1,19 @@
+-- to run this use \i path/to/file.sql
+-- If you get PERMISSION_DENIED on a windows machine, you have the wrong slashes in the file path
+
+DROP TABLE disliked_items CASCADE;
+DROP TABLE favorited_recipes CASCADE;
+DROP TABLE food_items CASCADE;
+DROP TABLE inventory_items CASCADE;
+DROP TABLE kitchen_tools CASCADE;
+DROP TABLE minor_items CASCADE;
+DROP TABLE password_reset_token CASCADE;
+DROP TABLE personal_recipes CASCADE;
+DROP TABLE recipe_items CASCADE;
+DROP TABLE recipe_kitchen_tools CASCADE;
+DROP TABLE recipe_kitchen_tools CASCADE;
+DROP TABLE recipe_reviews CASCADE;
+DROP TABLE recipes CASCADE;
+DROP TABLE substitutions CASCADE;
+DROP TABLE user_preferences CASCADE;
+DROP TABLE users CASCADE;
